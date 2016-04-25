a82c3e52f7dae0d5cadb3c589d3b3e00a9618f63
Richard Hodges <richard.hodges@researchresearch.com>
2016-04-25 14:15:22 +0100

    Work in progress commit

---
+++ desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
@@ -0,0 +1,16 @@
+package com.mygdx.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.mygdx.game.ImageLoader;
+import com.mygdx.game.MyGdxGame;
+import com.mygdx.game.UISimpleTest;
+import com.mygdx.game.UITest;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new ImageLoader(), config);
+//		new LwjglApplication(new MyGdxGame(), config);
+	}
+}
